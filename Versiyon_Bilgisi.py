51cc071c34ee3af3128a886644540e7a1d01e5e8
batubaykar <batubaykar2022@gmail.com>
2024-05-31 11:54:22 +0300

    -

--- Versiyon_Bilgisi.py
+++ Versiyon_Bilgisi.py
@@ -1,10 +1,19 @@
 class Versiyon_Bilgisi:
     major       = 0
     minor       = 1
-    build       = 1
+    build       = 2
     product_id  = 0
 
 #region VERSIYON GECMISI
+###########################################################
+# Versiyon : 0.1.2.0
+# Yazanlar : Batuhan Biber
+# Tarih    : 31.05.2024
+#
+# Gelismeler:
+# - Versiyon Bilgisindeki hata giderildi.
+#
+###########################################################
 
 ###########################################################
 # Versiyon : 0.1.1.0
@@ -13,6 +22,16 @@ class Versiyon_Bilgisi:
 #
 # Gelismeler:
 # - UI'a versiyon bilgisi eklendi.
+#
+###########################################################
+
+###########################################################
+# Versiyon : 0.1.1.0
+# Yazanlar : Batuhan Biber
+# Tarih    : 31.05.2024
+#
+# Gelismeler:
+# - privateGPT initlendi.
 #
 ###########################################################
 
